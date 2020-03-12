5706276b80955457025889ec40c6f11abd8619d7
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-12 14:40:40 -0300

    Services

--- src/main/java/br/com/rd/ecommerce/controller/CategoriaController.java
+++ src/main/java/br/com/rd/ecommerce/controller/CategoriaController.java
@@ -1,70 +1,69 @@
 package br.com.rd.ecommerce.controller;
 
 import br.com.rd.ecommerce.model.Categoria;
-import br.com.rd.ecommerce.repository.CategoriaRepository;
+import br.com.rd.ecommerce.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
-import java.util.List;
 
 
 @RestController
 public class CategoriaController {
-    @Autowired
-    private CategoriaRepository repository;
-
-    @PostMapping("/categoria")
-    public Categoria save(@RequestBody Categoria categoria) {
-        return repository.save(categoria);
-    }
-
-    //Mais de um parametro coloca uma barra e coloca o proximo parametro / Necessita de tratamento variavel obrigatoria
-    @GetMapping("/categoria/{idCategoria}")
-    public Categoria findById(@PathVariable("idCategoria") Long idCategoria) {
-        return repository.findById(idCategoria).get();
-    }
-
-    //Parametro opicional
-    @GetMapping("/categoria")
-    public Categoria findCategoriaById(@PathParam("id") Long id) {
-        return repository.findById(id).get();
-    }
-
-    @DeleteMapping("/categoria/{id}")
-    public void deleteById(@PathVariable("id") Long id) {
-        repository.deleteById(id);
-    }
-
+//    @Autowired
+//    private CategoriaService service;
 //
-
-    //Put alterar todos os campos, Patch alterar alguns apenas
-
-        /*
-    @PutMapping("/categoria")
-    public ResponseEntity<Categoria >alterar(@RequestBody Categoria categoria) {
-        Categoria categoriaEntity = repository.getOne(categoria.getCodigo());
-        categoriaEntity.setDescricao(categoria.getDescricao());
-        Categoria categoriaAtualizada = repository.save(categoriaEntity);
-        return ResponseEntity.ok().body(categoriaAtualizada);
-    }
-      */
-
-    @PutMapping("/categoria")
-    public Categoria alterar(@RequestBody Categoria categoria) {
-        Categoria categoriaEntity = repository.getOne(categoria.getCodigo());
-        categoriaEntity.setDescricao(categoria.getDescricao());
-        return repository.save(categoriaEntity);
-    }
-
-
-    @PatchMapping("/categoria")
-    public Categoria alteraCamposEspecificos(@RequestBody Categoria categoria) {
-        Categoria categoriaEntity = repository.getOne(categoria.getCodigo());
-        categoriaEntity.setDescricao(categoria.getDescricao());
-        return repository.save(categoriaEntity);
-    }
+//    @PostMapping("/categoria")
+//    public ResponseEntity salvar(@RequestBody Categoria categoria) {
+//        return service.salvar(categoria);
+//    }
+//
+//    //Mais de um parametro coloca uma barra e coloca o proximo parametro / Necessita de tratamento variavel obrigatoria
+//    @GetMapping("/categoria/{idCategoria}")
+//    public ResponseEntity buscaPorId(@PathVariable("idCategoria") Long idCategoria) {
+//        return service.buscaPorId(idCategoria);
+//    }
+//
+//    //Parametro opicional
+//    @GetMapping("/categoria")
+//    public ResponseEntity findCategoriaById(@PathParam("id") Long id) {
+//        return service.findCategoriaById(id);
+//    }
+//
+//    @DeleteMapping("/categoria/{id}")
+//    public void deleteById(@PathVariable("id") Long id) {
+//        service.deleteById(id);
+//    }
+//
+////
+//
+//    //Put alterar todos os campos, Patch alterar alguns apenas
+//
+//        /*
+//    @PutMapping("/categoria")
+//    public ResponseEntity<Categoria >alterar(@RequestBody Categoria categoria) {
+//        Categoria categoriaEntity = repository.getOne(categoria.getCodigo());
+//        categoriaEntity.setDescricao(categoria.getDescricao());
+//        Categoria categoriaAtualizada = repository.save(categoriaEntity);
+//        return ResponseEntity.ok().body(categoriaAtualizada);
+//    }
+//      */
+//
+//    @PutMapping("/categoria")
+//    public ResponseEntity alterar(@RequestBody Categoria categoria) {
+//        Categoria categoriaEntity = service.alterar(categoria.getCodigo());
+//        categoriaEntity.setDescricao(categoria.getDescricao());
+//        return service.alterar(categoriaEntity);
+//    }
+//
+//
+//    @PatchMapping("/categoria")
+//    public ResponseEntity alteraCamposEspecificos(@RequestBody Categoria categoria) {
+//        Categoria categoriaEntity = service.getOne(categoria.getCodigo());
+//        categoriaEntity.setDescricao(categoria.getDescricao());
+//        return service.save(categoriaEntity);
+//    }
 
 
 
--- src/main/java/br/com/rd/ecommerce/service/CategoriaService.java
+++ src/main/java/br/com/rd/ecommerce/service/CategoriaService.java
@@ -1,14 +1,45 @@
 package br.com.rd.ecommerce.service;
 
+import br.com.rd.ecommerce.model.Categoria;
+import br.com.rd.ecommerce.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service("CategoriaService")
 public class CategoriaService {
 
-    //TODO
+    @Autowired
+    CategoriaRepository repository;
 
-//    public ResponseEntity salvar(){
-//
-//    }
+    public ResponseEntity salvar(Categoria categoria){
+        return ResponseEntity.ok().body(repository.save(categoria));
+    }
+
+    public ResponseEntity buscaPorId(Long idCategoria) {
+        return ResponseEntity.ok().body(repository.findById(idCategoria).get());
+    }
+
+    public ResponseEntity findCategoriaById(Long id) {
+        return ResponseEntity.ok().body(repository.findById(id).get());
+    }
+
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public ResponseEntity alterar(Categoria categoria) {
+        Categoria categoriaEntity = repository.getOne(categoria.getCodigo());
+        categoriaEntity.setDescricao(categoria.getDescricao());
+        return ResponseEntity.ok().body(repository.save(categoria));
+    }
+
+
+
+    public ResponseEntity alteraCamposEspecificos(Categoria categoria) {
+        Categoria categoriaEntity = repository.getOne(categoria.getCodigo());
+        categoriaEntity.setDescricao(categoria.getDescricao());
+        return ResponseEntity.ok().body(repository.save(categoriaEntity));
+    }
 }
