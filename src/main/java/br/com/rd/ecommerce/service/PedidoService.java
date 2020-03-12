952f449dfc7186de7949dcaaeabaa50b74d8e7b8
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-12 09:18:32 -0300

    Criando Repositorio

---
+++ src/main/java/br/com/rd/ecommerce/service/PedidoService.java
@@ -0,0 +1,13 @@
+package br.com.rd.ecommerce.service;
+
+import br.com.rd.ecommerce.model.Pedido;
+import org.springframework.stereotype.Service;
+
+@Service("PedidoService")
+public class PedidoService {
+
+    public Pedido salvar(Pedido pedido){
+
+    }
+
+}
