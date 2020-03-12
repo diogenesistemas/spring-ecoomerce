23125c2bb8c6ac2db265c23c15ac53effb7349e6
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-03-12 09:20:15 -0300

    Injetando Pedido Repository na Service

--- src/main/java/br/com/rd/ecommerce/service/PedidoService.java
+++ src/main/java/br/com/rd/ecommerce/service/PedidoService.java
@@ -1,13 +1,16 @@
 package br.com.rd.ecommerce.service;
 
 import br.com.rd.ecommerce.model.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("PedidoService")
 public class PedidoService {
 
+    @Autowired
+    PedidoRepository repository;
     public Pedido salvar(Pedido pedido){
-
+        return repository.save(pedido);
     }
 
 }
